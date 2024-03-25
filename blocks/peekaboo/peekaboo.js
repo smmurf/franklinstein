6dec8910844e88087a40718de344e9681a4e07fb
Anton Vernihor <anton_vernihor@epam.com>
2024-03-25 15:33:55 +0200

    Playing around with custom blocks

--- blocks/peekaboo/peekaboo.js
+++ blocks/peekaboo/peekaboo.js
@@ -10,7 +10,7 @@ export default async function decorate(block) {
   }
   const button = document.createElement('button');
   button.textContent = el.textContent;
-  button.addEventListener('click', peekABoo);
+  button.addEventListener('click', peekABoo, {passive: true});
   block.replaceChildren();
   block.appendChild(button);
 }
