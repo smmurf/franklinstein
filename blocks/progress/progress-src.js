7e745fcab800ace15b6c753bddf5bb43a84254a9
Anton Vernihor <smmurf@gmail.com>
2024-03-27 16:17:36 +0200

    UDEx experiments

--- blocks/progress/progress-src.js
+++ blocks/progress/progress-src.js
@@ -8,6 +8,6 @@ export default function decorate(block) {
     const progress = el.textContent;
     block.replaceChildren();
     const pBar = document.createElement('udex-progress-bar');
-    pBar.setAttribute('value', progress);
+    pBar.setAttribute('value', 25);
     block.appendChild(pBar);
 }
